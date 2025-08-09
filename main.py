2914444c6fa54911cc82aa89bd685a83880383e3
am499702@gmail.com <am499702@gmail.com>
2025-08-09 12:25:42 +0530

    CORS Policy

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # 👇 Add this block to enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React frontend
+    allow_origins=["*"],  # React frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
